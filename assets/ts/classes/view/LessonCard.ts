644a1482f07b494275149aa5aee040e617889980
OficerKot <razyice@mail.ru>
2026-06-15 15:43:00 +1000

    added lesson dialog window
    Как у двфу

--- assets/ts/classes/view/LessonCard.ts
+++ assets/ts/classes/view/LessonCard.ts
@@ -1,12 +1,14 @@
 import { Lesson } from "../model/Lesson.js";
 
 export class LessonCard {
-  //в будущем карточка будет кликабельна, будет раскрываться подробная информация
-  private isClicked: Boolean = false;
   private divElem!: HTMLDivElement;
+  private dialogElem!: HTMLDialogElement;
 
   constructor(private lesson: Lesson) {
     this.createCard();
+    this.createDialog();
+
+    this.divElem.addEventListener("click", () => this.handleClick());
   }
 
   getDivElement(): HTMLDivElement {
@@ -17,16 +19,41 @@ export class LessonCard {
   private createCard() {
     //TODO: Добавить дизайн для карточки!
 
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.innerHTML = `
+    this.divElem = document.createElement("div");
+    this.divElem.classList.add("card");
+    this.divElem.innerHTML = `
     <div>${this.lesson.lessonName}</div>
     <div>${this.lesson.lessonType}</div>
-    <div>${this.lesson.teacher.last_name} ${this.lesson.teacher.first_name} ${this.lesson.teacher.middle_name}</div>
     <div>${this.lesson.group.name}</div>
     <div>${this.lesson.classroom.building + this.lesson.classroom.classroomNumber}</div>
   `;
+  }
 
-    this.divElem = card;
+  //**Создаёт диалоговое окно с полной информацией о занятии */
+  private createDialog() {
+    this.dialogElem = document.createElement("dialog");
+    this.dialogElem.innerHTML = `
+	<div>Дисциплина: ${this.lesson.lessonName}</div>
+    <div>Тип занятия: ${this.lesson.lessonType}</div>
+    <div>Преподаватель: ${this.lesson.teacher.last_name} ${this.lesson.teacher.first_name} ${this.lesson.teacher.middle_name}</div>
+    <div>Группа: ${this.lesson.group.name}, ${this.lesson.group.studentCount}чел.</div>
+    <div>Аудитория: ${this.lesson.classroom.building + this.lesson.classroom.classroomNumber}</div>
+	`;
+
+    const closeBtn = document.createElement("button");
+    closeBtn.textContent = "Закрыть";
+    closeBtn.addEventListener("click", () => this.dialogElem.close());
+
+    this.dialogElem.appendChild(closeBtn);
+    document.body.appendChild(this.dialogElem);
+  }
+
+  //**Управляет открытием/закрытием диалогового окна */
+  private handleClick() {
+    if (!this.dialogElem.open) {
+      this.dialogElem.showModal();
+    } else {
+      this.dialogElem.close();
+    }
   }
 }
