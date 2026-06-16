acf3b9455048d5203e777393a475d57bcab8dbd6
PancakeWithHoney <pavelliutar@gmail.com>
2026-06-16 11:36:26 +1000

    Update Schedule.sql

--- Schedule.sql
+++ Schedule.sql
@@ -1,1 +1,231 @@
--- Этот файл заменить на экспортированную из phpMyAdmin базу данных
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Июн 16 2026 г., 04:34
+-- Версия сервера: 5.6.51
+-- Версия PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `Schedule`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `disciplines`
+--
+
+CREATE TABLE `disciplines` (
+  `discipline_id` int(255) NOT NULL,
+  `discipline_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `groups`
+--
+
+CREATE TABLE `groups` (
+  `group_id` int(255) NOT NULL,
+  `group_code` int(255) NOT NULL,
+  `discipline_id` int(255) NOT NULL,
+  `students_count` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `lessons_periods`
+--
+
+CREATE TABLE `lessons_periods` (
+  `period_id` int(255) NOT NULL,
+  `time` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `lesson_card`
+--
+
+CREATE TABLE `lesson_card` (
+  `card_id` int(255) NOT NULL,
+  `discipline_id` int(255) NOT NULL,
+  `lesson_type_id` int(255) NOT NULL,
+  `group_id` int(255) NOT NULL,
+  `teacher_id` int(255) NOT NULL,
+  `room_id` int(255) NOT NULL,
+  `period_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `lesson_periods`
+--
+
+CREATE TABLE `lesson_periods` (
+  `period_id` int(255) NOT NULL,
+  `period` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `lesson_types`
+--
+
+CREATE TABLE `lesson_types` (
+  `lesson_type_id` int(255) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `room_id` int(255) NOT NULL,
+  `room_number` int(255) NOT NULL,
+  `room_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `free_space_count` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `teachers`
+--
+
+CREATE TABLE `teachers` (
+  `teacher_id` int(255) NOT NULL,
+  `first_name` int(255) NOT NULL,
+  `last_name` int(255) NOT NULL,
+  `type` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `disciplines`
+--
+ALTER TABLE `disciplines`
+  ADD PRIMARY KEY (`discipline_id`);
+
+--
+-- Индексы таблицы `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`group_id`),
+  ADD KEY `discipline_id` (`discipline_id`);
+
+--
+-- Индексы таблицы `lessons_periods`
+--
+ALTER TABLE `lessons_periods`
+  ADD PRIMARY KEY (`period_id`);
+
+--
+-- Индексы таблицы `lesson_card`
+--
+ALTER TABLE `lesson_card`
+  ADD PRIMARY KEY (`card_id`),
+  ADD KEY `lesson_type_id` (`lesson_type_id`),
+  ADD KEY `group_id` (`group_id`),
+  ADD KEY `teacher_id` (`teacher_id`),
+  ADD KEY `room_id` (`room_id`),
+  ADD KEY `lesson_name_id` (`discipline_id`),
+  ADD KEY `period_id` (`period_id`);
+
+--
+-- Индексы таблицы `lesson_periods`
+--
+ALTER TABLE `lesson_periods`
+  ADD PRIMARY KEY (`period_id`);
+
+--
+-- Индексы таблицы `lesson_types`
+--
+ALTER TABLE `lesson_types`
+  ADD PRIMARY KEY (`lesson_type_id`);
+
+--
+-- Индексы таблицы `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`room_id`);
+
+--
+-- Индексы таблицы `teachers`
+--
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`teacher_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `group_id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `lessons_periods`
+--
+ALTER TABLE `lessons_periods`
+  MODIFY `period_id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `lesson_card`
+--
+ALTER TABLE `lesson_card`
+  MODIFY `card_id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `lesson_periods`
+--
+ALTER TABLE `lesson_periods`
+  MODIFY `period_id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `lesson_types`
+--
+ALTER TABLE `lesson_types`
+  MODIFY `lesson_type_id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `room_id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `teachers`
+--
+ALTER TABLE `teachers`
+  MODIFY `teacher_id` int(255) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
